38fbd0549df6e22e57fcd4db3c2d112d30699c19
eniola <eniolafaithful57@gmail.com>
2025-11-05 18:48:39 +0100

    Making websocker server entrypoint for Nextjs
    I tried to add a custom server for the websocket functionality
    previoulsy, but I wasn't able to get it to work for some reason, so I just resorted to
    setting up an express server as a separate folder.
    
    But I just came about an article from socket.io on an easy
    way to setup websockets in a Next.js project, and I decided to try it out.

---
+++ server.ts
@@ -0,0 +1,45 @@
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("message", (msg) => {
+      console.log("message: " + msg);
+      io.emit("message", msg); // Broadcast message to all connected clients
+      io.emit("userCount", io.sockets.sockets.size); // Emit user count
+    });
+
+    socket.on("notice", (notice) => {
+      console.log("notice: " + notice);
+      io.emit("notice", notice); // Broadcast notice to all connected clients
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+      io.emit("userCount", io.sockets.sockets.size); // Emit user count
+    });
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
